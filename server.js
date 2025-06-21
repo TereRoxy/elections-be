1ef39f9e71dc9ef4ad1529e12020c81af6bc709b
Terebent Roxana <roxana.terebent@stud.ubbcluj.ro>
2025-06-21 16:38:18 +0300

    update server

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000, // 24 hours
         sameSite: 'none', // Allow cross-origin cookies
         httpOnly: true // Prevent client-side access to cookies
