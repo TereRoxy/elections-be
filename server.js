4c23689d8770378dee47c2880442af4b6c163048
Terebent Roxana <roxana.terebent@stud.ubbcluj.ro>
2025-06-21 16:15:06 +0300

    modify cors

--- server.js
+++ server.js
@@ -96,7 +96,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: process.env.NODE_ENV === 'production',
-        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        sameSite: 'none' // Allow cross-origin cookies
     }
 }));
 
